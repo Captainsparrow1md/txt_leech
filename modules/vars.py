e166611d505745a7d2d4876b57cb9acae98e6777
Captainsparrow1md <164557863+Captainsparrow1md@users.noreply.github.com>
2024-04-15 00:47:53 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "23549941"
 api_hash = "0eb3903dcc268ef7c2ec9b53dd6005ff"
-bot_token = "6565682263:AAFX_qjEm061WwcTg5z3SLEZuddEiLZnXLM"
+bot_token = "6822276885:AAFaJTTN6tvf1ybyLAk2KENl9pZ5o6yK96M"
